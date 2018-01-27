92addb08400f7e3f2612ddfd95b9030111a9f724
Marijn Huizendveld <marijn@round84.com>
2018-01-27 11:53:09 +0100

    :smile: Improve error message in case of invalid usage.

--- code-generation-messages/bin/generate-code.php
+++ code-generation-messages/bin/generate-code.php
@@ -6,9 +6,24 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Acme\CodeGeneration\CodeGenerator;
 use Symfony\Component\Yaml\Yaml;
 
+if (count($argv) < 2 || count($argv) > 2)
+{
+    echo "\033[0;31mInvalid usage of script.\033[0m\n";
+    echo "\n";
+    echo "You have to provide:\n";
+    echo "\n";
+    echo " - dsl (e.g. example-dsl.yaml)\n";
+    echo "\n";
+    echo "Example:\n";
+    echo "\n";
+    echo " $\033[0;32m bin/generate-code.php src/Acme/OnlineShop/messages.yaml\033[0m\n";
+    echo "\n";
+    die(1);
+}
+
 $cg = new CodeGenerator;
 $dslUri = $argv[1];
-$dslFile = file_get_contents($dslUri);
+$dslFile = @file_get_contents($dslUri);
 $dsl = Yaml::parse($dslFile);
 
 echo $cg->generate($dsl), PHP_EOL;
