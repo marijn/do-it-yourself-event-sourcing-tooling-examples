6605d22f8107ab1c902e1451271c92eb8ddad726
Marijn Huizendveld <marijn@round84.com>
2018-01-27 12:16:42 +0100

    :memo: Document todo.

--- code-generation-messages/bin/compile.sh
+++ code-generation-messages/bin/compile.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
+# TODO: Only pipe to `src/Acme/OnlineShop/messages.php` when command returned with exit code 0
 bin/generate-code.php src/Acme/OnlineShop/messages.yaml > src/Acme/OnlineShop/messages.php
