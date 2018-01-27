a93c9c43de092753764138c1493c9f91cd910a35
Marijn Huizendveld <marijn@round84.com>
2018-01-27 16:53:50 +0100

    :alembic: Extract method.

--- code-generation-messages/src/Acme/Infra/EventSourcing/CodeGeneration/CodeGenerator.php
+++ code-generation-messages/src/Acme/Infra/EventSourcing/CodeGeneration/CodeGenerator.php
@@ -168,28 +168,34 @@ DOCBLOCK;
 
         foreach ($moduleSpecification['commands'] as $commandClassName => $commandSpecification)
         {
-            $docBlocks = [];
-
-            if (array_key_exists('doc', $commandSpecification))
-            {
-                $docBlocks[] = $commandSpecification['doc'];
-                $docBlocks[] = '';
-            }
-
-            $docBlocks[] = '@api';
-            $docBlocks[] = '@category generated';
-            $command = <<<PHP
-{$this->generateDockBlocks(... $docBlocks)}
-final class {$commandClassName} implements \Acme\Infra\EventSourcing\Command {
-{$this->generateMessageConstructor($commandSpecification)}
-{$this->generateMessageAttributes($commandSpecification)}
-}
-PHP;
+            $command = $this->generateCommand($commandClassName, $commandSpecification);
             $commands[] = $command;
         }
 
         $commandCode = implode(PHP_EOL, $commands);
 
         return $commandCode;
+    }
+
+    private function generateCommand (string $commandClassName, array $commandSpecification): string {
+        $docBlocks = [];
+
+        if (array_key_exists('doc', $commandSpecification))
+        {
+            $docBlocks[] = $commandSpecification['doc'];
+            $docBlocks[] = '';
+        }
+
+        $docBlocks[] = '@api';
+        $docBlocks[] = '@category generated';
+        $command = <<<PHP
+{$this->generateDockBlocks(... $docBlocks)}
+final class {$commandClassName} implements \Acme\Infra\EventSourcing\Command {
+{$this->generateMessageConstructor($commandSpecification)}
+{$this->generateMessageAttributes($commandSpecification)}
+}
+PHP;
+
+        return $command;
     }
 }
