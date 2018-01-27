8748215ba2327a5e8543e1e4edf84161aa73665a
Marijn Huizendveld <marijn@round84.com>
2018-01-27 12:01:29 +0100

    :gem: Write explicitly to stdout.

--- code-generation-messages/bin/generate-code.php
+++ code-generation-messages/bin/generate-code.php
@@ -9,18 +9,20 @@ use Symfony\Component\Yaml\Yaml;
 const errorOnInvalidUsage = 1;
 const errorOnFileReadingProblems = 2;
 
+$stdout = fopen('php://stdout', 'w');
+
 if (count($argv) < 2 || count($argv) > 2)
 {
-    echo "\033[0;31mInvalid usage of script.\033[0m\n";
-    echo "\n";
-    echo "You have to provide:\n";
-    echo "\n";
-    echo " - dsl (e.g. example-dsl.yaml)\n";
-    echo "\n";
-    echo "Example:\n";
-    echo "\n";
-    echo " $\033[0;32m bin/generate-code.php src/Acme/OnlineShop/messages.yaml\033[0m\n";
-    echo "\n";
+    fwrite($stdout, "\033[0;31mInvalid usage of script.\033[0m\n");
+    fwrite($stdout, "\n");
+    fwrite($stdout, "You have to provide:\n");
+    fwrite($stdout, "\n");
+    fwrite($stdout, " - dsl (e.g. example-dsl.yaml)\n");
+    fwrite($stdout, "\n");
+    fwrite($stdout, "Example:\n");
+    fwrite($stdout, "\n");
+    fwrite($stdout, " $\033[0;32m bin/generate-code.php src/Acme/OnlineShop/messages.yaml\033[0m\n");
+    fwrite($stdout, "\n");
     die(errorOnInvalidUsage);
 }
 
@@ -30,10 +32,10 @@ $dslFile = @file_get_contents($dslUri);
 
 if (false === $dslFile)
 {
-    echo "\033[0;31mFile '{$dslUri}' could not be read.\033[0m\n";
+    fwrite($stdout, "\033[0;31mFile '{$dslUri}' could not be read.\033[0m\n");
     die(errorOnFileReadingProblems);
 }
 
 $dsl = Yaml::parse($dslFile);
 
-echo $cg->generate($dsl);
+fwrite($stdout, $cg->generate($dsl));
