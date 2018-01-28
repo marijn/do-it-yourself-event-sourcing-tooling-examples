526f716656e757cbecfac9e3ad3de5b1720974d5
Marijn Huizendveld <marijn@round84.com>
2018-01-28 11:51:02 +0100

    :gem: Introduce `Message` concept.

--- code-generation-messages/src/Acme/Infra/EventSourcing/Event.php
+++ code-generation-messages/src/Acme/Infra/EventSourcing/Event.php
@@ -5,6 +5,6 @@ namespace Acme\Infra\EventSourcing;
 /**
  * @copyright Marijn Huizendveld 2018. All rights reserved.
  */
-interface Event {
+interface Event extends Message {
 
 }
--- code-generation-messages/src/Acme/Infra/EventSourcing/Message.php
+++ code-generation-messages/src/Acme/Infra/EventSourcing/Message.php
@@ -5,6 +5,6 @@ namespace Acme\Infra\EventSourcing;
 /**
  * @copyright Marijn Huizendveld 2018. All rights reserved.
  */
-interface Command extends Message {
+interface Message {
 
 }
