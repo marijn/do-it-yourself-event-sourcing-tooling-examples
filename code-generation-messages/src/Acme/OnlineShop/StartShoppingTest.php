6260924217bacda33837b5c5d18e698cef747212
Marijn Huizendveld <marijn@round84.com>
2018-01-28 21:19:04 +0100

    :blue_book: Rename variable.

--- code-generation-messages/src/Acme/OnlineShop/StartShoppingTest.php
+++ code-generation-messages/src/Acme/OnlineShop/StartShoppingTest.php
@@ -33,13 +33,13 @@ final class StartShoppingTest extends EventSourcedCommandHandlerTestCase {
         $cartId = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa';
         $customerId = 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb';
         $earlierStartTime = '2017-10-07T09:12:14.999999+0000';
-        $laterStartTime = '2017-10-07T09:12:15.999999+0000';
+        $startTime = '2017-10-07T09:12:15.999999+0000';
 
         $this->scenario
 
             ->given(new CustomerStartedShopping($cartId, $customerId, $earlierStartTime))
 
-            ->when(new StartShopping($cartId, $customerId, $laterStartTime))
+            ->when(new StartShopping($cartId, $customerId, $startTime))
 
             ->thenNothing()
 
