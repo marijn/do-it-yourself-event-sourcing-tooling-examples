6499ce1b94d12f7a7a8f8d944f6edcd1bf3fd341
Marijn Huizendveld <marijn@round84.com>
2018-01-28 21:19:32 +0100

    :recycle: Replace manual construction with factory.

--- code-generation-messages/src/Acme/OnlineShop/StartShoppingTest.php
+++ code-generation-messages/src/Acme/OnlineShop/StartShoppingTest.php
@@ -32,12 +32,11 @@ final class StartShoppingTest extends EventSourcedCommandHandlerTestCase {
     function ignore StartShopping when CustomerStartedShopping already (): void {
         $cartId = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa';
         $customerId = 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb';
-        $earlierStartTime = '2017-10-07T09:12:14.999999+0000';
         $startTime = '2017-10-07T09:12:15.999999+0000';
 
         $this->scenario
 
-            ->given(new CustomerStartedShopping($cartId, $customerId, $earlierStartTime))
+            ->given(CustomerStartedShopping::withCartId($cartId))
 
             ->when(new StartShopping($cartId, $customerId, $startTime))
 
