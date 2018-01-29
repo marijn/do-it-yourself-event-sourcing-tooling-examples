b3fb73245ddf02ed11f453bf5b194e2e415f0303
Marijn Huizendveld <marijn@round84.com>
2018-01-29 20:15:55 +0100

    :alembic: Extract factory.

--- code-generation-messages/public/index.php
+++ code-generation-messages/public/index.php
@@ -1,12 +1,8 @@
 <?php declare(strict_types = 1);
 
 use Acme\Application\Dependencies;
-use Acme\Application\OnlineShop\ProductsController;
-use FastRoute\RouteCollector;
-use function FastRoute\simpleDispatcher;
 use Middlewares\FastRoute;
 use Middlewares\RequestHandler;
-use mindplay\middleman\Delegate;
 use mindplay\middleman\Dispatcher as HttpMiddlewareDispatcher;
 use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
@@ -15,12 +11,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $request = ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
 $dependencies = new Dependencies();
-$templatingEngine = $dependencies->templatingEngine();
-$productRepository = $dependencies->productRepository();
-$router = simpleDispatcher(function (RouteCollector $routing) use ($productRepository, $templatingEngine)
-{
-    $routing->addRoute('GET', '/', new Delegate([new ProductsController($productRepository, $templatingEngine), 'handle']));
-});
+$router = $dependencies->router();
 $middlewares = [
     new FastRoute($router),
     new RequestHandler(),
--- code-generation-messages/src/Acme/Application/Dependencies.php
+++ code-generation-messages/src/Acme/Application/Dependencies.php
@@ -3,7 +3,12 @@
 namespace Acme\Application;
 
 use Acme\Application\OnlineShop\HardCodedProductRepository;
+use Acme\Application\OnlineShop\ProductsController;
 use Acme\OnlineShop\ProductRepository;
+use FastRoute\Dispatcher as RouteDispatcher;
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+use mindplay\middleman\Delegate;
 use Symfony\Component\Templating\EngineInterface as TemplatingEngine;
 use Symfony\Component\Templating\Loader\FilesystemLoader;
 use Symfony\Component\Templating\PhpEngine;
@@ -20,5 +25,15 @@ final class Dependencies {
 
     function productRepository (): ProductRepository {
         return new HardCodedProductRepository;
+    }
+
+    function router (): RouteDispatcher {
+        $productRepository = $this->productRepository();
+        $templatingEngine = $this->templatingEngine();
+
+        return simpleDispatcher(function (RouteCollector $routing) use ($productRepository, $templatingEngine)
+        {
+            $routing->addRoute('GET', '/', new Delegate([new ProductsController($productRepository, $templatingEngine), 'handle']));
+        });
     }
 }
