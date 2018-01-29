41a1bef07350ad398791a6b6e13669152ff2cd18
Marijn Huizendveld <marijn@round84.com>
2018-01-29 11:03:08 +0100

    :mag: `PlaceOrder` should result in `OrderWasPlaced`.

---
+++ code-generation-messages/src/Acme/OnlineShop/PlaceOrderTest.php
@@ -0,0 +1,170 @@
+<?php declare(strict_types=1);
+
+namespace Acme\OnlineShop;
+
+use Acme\Infra\EventSourcing\Testing\EventSourcedCommandHandlerTestCase;
+use Acme\Infra\Standards;
+use Faker\Factory;
+
+/**
+ * @copyright Marijn Huizendveld 2018. All rights reserved.
+ */
+final class PlaceOrderTest extends EventSourcedCommandHandlerTestCase {
+
+    /**
+     * @test
+     */
+    function CustomerPlacedOrder when PlaceOrder (): void {
+        $faker = Factory::create();
+
+        $cartId = $faker->uuid;
+        $customerId = $faker->uuid;
+        $orderId = $faker->uuid;
+        $currency = $faker->currencyCode;
+        $sku1stProduct = $faker->ean13;
+        $priceInCents1stProduct = $faker->numberBetween(499, 12999);
+        $sku2ndProduct = $faker->ean13;
+        $priceInCents2ndProduct = $faker->numberBetween(499, 12999);
+        $orderTime = $faker->dateTimeThisYear('3 months ago', 'UTC')->format(Standards::dateTimeFormat);
+        $orderLines = [
+            [
+                'sku' => $sku1stProduct,
+                'quantity' => 1,
+                'priceInCents' => $priceInCents1stProduct,
+                'currency' => $currency,
+            ],[
+                'sku' => $sku2ndProduct,
+                'quantity' => 1,
+                'priceInCents' => $priceInCents2ndProduct,
+                'currency' => $currency,
+            ],
+        ];
+
+        $this->scenario
+            ->given(
+                CustomerStartedShopping::withCartId($cartId)
+                    ->andWithCustomerId($customerId),
+
+                ProductWasAddedToCart::withCartId($cartId)
+                    ->andWithSku($sku1stProduct)
+                    ->andWithPriceInCents($priceInCents1stProduct)
+                    ->andWithCurrency($currency),
+
+                ProductWasAddedToCart::withCartId($cartId)
+                    ->andWithSku($sku2ndProduct)
+                    ->andWithPriceInCents($priceInCents2ndProduct)
+                    ->andWithCurrency($currency)
+            )
+            ->when(new PlaceOrder($cartId, $orderTime))
+            ->then(new CustomerPlacedOrder($cartId, $customerId, $orderId, $orderLines, $orderTime))
+            ->assert();
+    }
+
+    /**
+     * @test
+     */
+    function order lines are combined in CustomerPlacedOrder when PlaceOrder (): void {
+        $faker = Factory::create();
+
+        $cartId = $faker->uuid;
+        $customerId = $faker->uuid;
+        $orderId = $faker->uuid;
+        $currency = $faker->currencyCode;
+        $sku1stProduct = $faker->ean13;
+        $priceInCents1stProduct = $faker->numberBetween(499, 12999);
+        $sku2ndProduct = $faker->ean13;
+        $priceInCents2ndProduct = $faker->numberBetween(499, 12999);
+        $orderTime = $faker->dateTimeThisYear('3 months ago', 'UTC')->format(Standards::dateTimeFormat);
+        $orderLines = [
+            [
+                'sku' => $sku1stProduct,
+                'quantity' => 2,
+                'priceInCents' => $priceInCents1stProduct,
+                'currency' => $currency,
+            ],[
+                'sku' => $sku2ndProduct,
+                'quantity' => 1,
+                'priceInCents' => $priceInCents2ndProduct,
+                'currency' => $currency,
+            ],
+        ];
+
+        $this->scenario
+            ->given(
+                CustomerStartedShopping::withCartId($cartId)
+                    ->andWithCustomerId($customerId),
+
+                ProductWasAddedToCart::withCartId($cartId)
+                    ->andWithSku($sku1stProduct)
+                    ->andWithPriceInCents($priceInCents1stProduct)
+                    ->andWithCurrency($currency),
+
+                ProductWasAddedToCart::withCartId($cartId)
+                    ->andWithSku($sku1stProduct)
+                    ->andWithPriceInCents($priceInCents1stProduct)
+                    ->andWithCurrency($currency),
+
+                ProductWasAddedToCart::withCartId($cartId)
+                    ->andWithSku($sku2ndProduct)
+                    ->andWithPriceInCents($priceInCents2ndProduct)
+                    ->andWithCurrency($currency)
+            )
+            ->when(new PlaceOrder($cartId, $orderTime))
+            ->then(new CustomerPlacedOrder($cartId, $customerId, $orderId, $orderLines, $orderTime))
+            ->assert();
+    }
+
+    /**
+     * @test
+     */
+    function ignore PlaceOrder when CustomerPlacedOrder already (): void {
+        $faker = Factory::create();
+
+        $cartId = $faker->uuid;
+        $customerId = $faker->uuid;
+        $orderId = $faker->uuid;
+        $currency = $faker->currencyCode;
+        $sku1stProduct = $faker->ean13;
+        $priceInCents1stProduct = $faker->numberBetween(499, 12999);
+        $sku2ndProduct = $faker->ean13;
+        $priceInCents2ndProduct = $faker->numberBetween(499, 12999);
+        $orderTime = $faker->dateTimeThisYear('3 months ago', 'UTC')->format(Standards::dateTimeFormat);
+        $orderLines = [
+            [
+                'sku' => $sku1stProduct,
+                'quantity' => 1,
+                'priceInCents' => $priceInCents1stProduct,
+                'currency' => $currency,
+            ],[
+                'sku' => $sku2ndProduct,
+                'quantity' => 1,
+                'priceInCents' => $priceInCents2ndProduct,
+                'currency' => $currency,
+            ],
+        ];
+
+        $this->scenario
+            ->given(
+                CustomerStartedShopping::withCartId($cartId)
+                    ->andWithCustomerId($customerId),
+
+                ProductWasAddedToCart::withCartId($cartId)
+                    ->andWithSku($sku1stProduct)
+                    ->andWithPriceInCents($priceInCents1stProduct)
+                    ->andWithCurrency($currency),
+
+                ProductWasAddedToCart::withCartId($cartId)
+                    ->andWithSku($sku2ndProduct)
+                    ->andWithPriceInCents($priceInCents2ndProduct)
+                    ->andWithCurrency($currency),
+
+                CustomerPlacedOrder::withCartId($cartId)
+                    ->andWithOrderId($orderId)
+                    ->andWithOrderLines($orderLines)
+                    ->andWithOrderPlacedAt($orderTime)
+            )
+            ->when(new PlaceOrder($cartId, $orderTime))
+            ->thenNothing()
+            ->assert();
+    }
+}
