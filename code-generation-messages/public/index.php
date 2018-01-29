04875bcda3b6607e434104f8e5119c96b468001a
Marijn Huizendveld <marijn@round84.com>
2018-01-29 20:21:41 +0100

    :gem: Changes order of creation.

--- code-generation-messages/public/index.php
+++ code-generation-messages/public/index.php
@@ -5,8 +5,8 @@ use Zend\Diactoros\ServerRequestFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$request = ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
 $dependencies = new Dependencies();
+$request = ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
 $dispatcher = $dependencies->httpRequestDispatcher();
 $emitter = $dependencies->responseEmitter();
 
